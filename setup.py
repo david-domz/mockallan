a7864457ac489d9dccc5e91aafd8f99350e76913
David D <david.7b8@gmail.com>
2023-09-17 01:38:37 +0200

    setup.py updated, added .gitignore

--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
 from setuptools import setup
 
 
-VERSION = '0.1'
-
 setup(
-	name="mock-http-server",
-	version=VERSION,
-	author="vurutal (David D.)",
+	name="mockallan",
+	version='0.0.1',
+	author="David Domínguez",
 	author_email="<david.7b8@gmail.com>",
-	description='Mock HTTP server',
-	py_modules=['mock_http_server'],
+	description='Versatile HTTP server mock designed for use as a substitute for a production HTTP server within a testing environment.',
+	py_modules=['mockallan', 'app_handler', 'history', 'request', 'stub_config'],
 	package_dir={'': 'src'},
 	long_description_content_type="text/markdown",
 	long_description='A configurable HTTP server mock that can be use in integration tests.',
 	install_requires=[],
 	keywords=['python', 'http', 'REST', 'mock', 'test', 'pytest'],
 	classifiers=[
-		"Development Status :: 1 - Planning",
+		"Development Status :: 4 - Beta",
+		"Environment :: Console",
 		"Intended Audience :: Developers",
-		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.10",
+		"Topic :: Software Development :: Testing",
+		"Topic :: Software Development :: Testing :: Mocking",
+		"Topic :: Internet :: WWW/HTTP",
+		"Topic :: Software Development :: Libraries :: Python Modules",
 		"Operating System :: Unix",
 		"Operating System :: MacOS :: MacOS X",
 		"Operating System :: Microsoft :: Windows",
+		"Operating System :: OS Independent",
 	]
 )
